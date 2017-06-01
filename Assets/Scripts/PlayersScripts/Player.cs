5c4deafa419b6f977076095dc37e6d14bc6dc624
marcelobonow <marcelobonowott@hotmail.com>
2017-06-01 09:18:19 -0300

    Health bar

--- Assets/Scripts/PlayersScripts/Player.cs
+++ Assets/Scripts/PlayersScripts/Player.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
     public Arrow arrow; //scripts que descendem deste terão um script que descende de arrow
                         //ou seja, cada personagem tem suas caracteristicas e uma flecha com suas caracteristicas
+    public string name = "Player";
+    private float maxHealth = 15f;
     private float health = 15f;
-	//healthbar
+    [SerializeField]
+    private Image healthImage;      //Imagem que se refere a sua vida
 
     public void TakeDamage(float _damage)
     {
@@ -14,5 +18,6 @@ public class Player : MonoBehaviour {
             health = 0;
             GameManager.Die(gameObject.name);
         }
+        healthImage.fillAmount = health / maxHealth; 
     }
 }
