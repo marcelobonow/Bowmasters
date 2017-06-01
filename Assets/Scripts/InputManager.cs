ba5dbe08d91fea9d266f5b3420ca4a59f17f6946
marcelobonow <marcelobonowott@hotmail.com>
2017-06-01 04:25:36 -0300

    Input changed

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -20,22 +20,23 @@ public class InputManager : MonoBehaviour {
 
         if (GameManager.staticStage == GameManager.Stage.Player)
         {
-            if (!hasSnap && CrossPlatformInputManager.GetButtonDown("Fire1"))
-            {
-                snapPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//varia a força e angulo baseado
-                hasSnap = true;                                                    //em onde a pessoa clicou
-            }
             if (hasSnap)
             {
                 //Angulo é calculado pela diferença no eixo y e a força na diferença do eixo x
-                Vector3 newMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                float angle = (snapPosition.y - newMousePosition.y)/5; //mudar depois
-                float shotPower = Mathf.Abs((snapPosition.x - newMousePosition.x)*4f);
+                Vector3 newMousePosition = Input.mousePosition;
+                float angle = (snapPosition.y - newMousePosition.y)/200; //mudar depois
+                Debug.Log("angle: " + angle);
+                float shotPower = Mathf.Abs((snapPosition.x - newMousePosition.x)/20);
                 if (shotPower > 20)
                     shotPower = 20;
                 Camera.main.orthographicSize = (shotPower/8f)+ 3;
                 GameManager.SetAngle(angle);
                 GameManager.SetShotPower(shotPower);
+            }
+            if (!hasSnap && CrossPlatformInputManager.GetButtonDown("Fire1"))
+            {
+                snapPosition = Input.mousePosition;//varia a força e angulo baseado
+                hasSnap = true;                                                    //em onde a pessoa clicou
             }
             if (CrossPlatformInputManager.GetButtonUp("Fire1"))
             {
