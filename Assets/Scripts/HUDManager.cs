7fa9adefb83baf97bc9f39fbb24ca1747924ce09
Marcelo Bonow <marcelobonowott@hotmail.com>
2020-01-08 20:59:27 -0300

    HUD working with wide screens

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,6 +1,7 @@
 ﻿//Script responsavel pelo controle da janela que aparece quando o player mirar, que lhe informa o angulo e a força
 using UnityEngine.UI;
 using UnityEngine;
+using System.Globalization;
 
 public class HUDManager : MonoBehaviour
 {
@@ -22,28 +23,28 @@ public class HUDManager : MonoBehaviour
         rectTransform.position = position;
         //Esse bloco de if/else if verifica se algum valor esta fora da tela, se estiver,
         //ele o corrige para dentro da tela
-        if ((position.x - rectTransform.rect.width / 2) < 0)
+        if((position.x - rectTransform.rect.width / 2) < 0)
         {
             rectTransform.position = new Vector2(rectTransform.rect.width / 2 + 3, rectTransform.position.y);
         }
-        else if ((position.x + rectTransform.rect.width / 2 + 3) > Screen.width)
+        else if((position.x + rectTransform.rect.width / 2 + 3) > Screen.width)
         {
             rectTransform.position = new Vector2(Screen.width - (rectTransform.rect.width / 2 + 3), rectTransform.position.y);
         }
-        if ((position.y + rectTransform.rect.height / 2 + 3) > Screen.height)
+        if((position.y + rectTransform.rect.height / 2 + 3) > Screen.height)
         {
-            rectTransform.position = new Vector2(rectTransform.position.x,Screen.height - (rectTransform.rect.height / 2 + 3));
+            rectTransform.position = new Vector2(rectTransform.position.x, Screen.height - (rectTransform.rect.height / 2 + 3));
         }
-        else if ((position.y - rectTransform.rect.height / 2) < 0)
+        else if((position.y - rectTransform.rect.height / 2) < 0)
         {
-            rectTransform.position = new Vector2(rectTransform.position.x,rectTransform.rect.height / 2 + 3);
+            rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.rect.height / 2 + 3);
         }
     }
     //Mostra no PopUp os valores do angulo e da força
     public void UpdatePopUp(float _angle, float _power)
     {
-        angleText.text = Mathf.Round((_angle * Mathf.Rad2Deg)).ToString() + "°";
-        powerText.text = _power.ToString();
+        angleText.text = Mathf.RoundToInt(_angle * Mathf.Rad2Deg).ToString() + "°";
+        powerText.text = _power.ToString("F2");
     }
     //PopUp visivel
     public void EnablePopUp()
